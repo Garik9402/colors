1941353d1d87868f95fad55ecf573089d89e148a
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-09-02 19:35:25 +0300

    in the process;)

--- src/js/script.js
+++ src/js/script.js
@@ -1,87 +1,197 @@
 
- // настройки слайдера
-$(document).ready(function() {
+// настройки слайдера
+$(document).ready(function () {
    $(".slider").slick({
-       arrows: true,
-       dots: true,
-       slidesToShow: 1,
-       speed: 1000,
-       autoplay: true,
-       slidesToScroll: 1,
-     
+      arrows: true,
+      dots: true,
+      slidesToShow: 1,
+      speed: 1000,
+      autoplay: true,
+      slidesToScroll: 1,
+
    });
 });
 
 // активный checkbox
 const checkboxNode = document.querySelectorAll('.checkbox')
-checkboxNode.forEach(function(elem){
-   elem.addEventListener('click', function(){
-      checkboxNode.forEach(function (elem){
+checkboxNode.forEach(function (elem) {
+   elem.addEventListener('click', function () {
+      checkboxNode.forEach(function (elem) {
          elem.classList.remove('checkbox--js-active')
       })
       this.classList.add('checkbox--js-active')
    })
 })
 // добавление товара в корзину + 'количество товара в корзине'
-const countProductsNode = document.querySelector('.header__basket-count')
-const proguctsAdd = document.querySelectorAll('.product__add')
- const productsNode = document.querySelectorAll('.product')
- const basketProductNode = document.querySelectorAll('.basket-popup__product')
-const amoutProductsNode = document.querySelectorAll('.basket-popup__product-amout')
-let count = 0;
-const addProducts = () => {
-   proguctsAdd.forEach(function(e){
-    e.addEventListener('click', function(){
-      let index = this.dataset.id
-      count++
-         basketProductNode.forEach(function(elem){
-         basketProductNode[index].classList.add('basket-popup__product--js-add')
-      })
-             
-   })
-   })
-}
-addProducts()
+
+const basketProductsNode = document.querySelector('.basket-popup__products')
+window.addEventListener('click', (event) => {
+   if (event.target.hasAttribute('data-product')) {
+      const product = event.target.closest('.product')
+
+      const productInfo = {
+         id: product.dataset.id,
+         imgSrc: product.querySelector('.product__img').getAttribute('src'),
+         description: product.querySelector('.product__description').innerText,
+         price: product.querySelector('.product__price').innerText,
+      };
+      // проверка есть ли уже товар в корзине 
+      const itemInProducts = basketProductsNode.querySelector(`[data-id="${productInfo.id}"]`)
+      if (itemInProducts) {
+         false
+      }
+      else {
+
+         const productItemsHtml = `<div class="basket-popup__product" data-id="${productInfo.id}">
+             <img class="basket-popup__product-img" src="${productInfo.imgSrc}" alt="${productInfo.description}" >
+            <div class="basket-popup__product-content">
+               <p class="basket-popup__product-description">${productInfo.description}</p>
+               <p class="basket-popup__product-price">${productInfo.price}</p>
+            </div>
+            <div class="basket-popup__product-amout-w">
+               <div class="basket-popup__product-minus" data-action='minus'>-</div>
+               <div class="basket-popup__product-amout" data-amout>1</div>
+               <div class="basket-popup__product-plus" data-action='plus'>+</div>
+            </div>
+            <div class="basket-popup__product-delete" data-close='del'>
+               <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <g opacity="0.2">
+                     <path d="M18 6L6 18" stroke="#1F2020" stroke-width="1.4" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                     <path d="M6 6L18 18" stroke="#1F2020" stroke-width="1.4" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                  </g>
+               </svg>
+            </div>
+      </div >`;
+         basketProductsNode.insertAdjacentHTML('beforeend', productItemsHtml)
+      }
+
+      productBasketStatus();
+      calc();
+
+   }
+
+})
 
 // открытие и закрыьте корзины
- const basketPopupNode = document.querySelector('.basket-popup')
- const basketNode = document.querySelector('.header__basket')
- const basketBodyNode = document.querySelector('.basket-popup__body')
+const basketPopupNode = document.querySelector('.basket-popup')
+const basketNode = document.querySelector('.header__basket')
+const basketBodyNode = document.querySelector('.basket-popup__body')
 const closeBasketNode = document.querySelector('.basket-popup__close')
- window.addEventListener('click', function(e) {
-             if (basketNode.contains(e.target)||(basketBodyNode.contains(e.target))) {
-               basketPopupNode.classList.add('basket-popup--js-open')
-               document.body.style.overflow='hidden'
-             }else {
-                basketPopupNode.classList.remove('basket-popup--js-open')
-                document.body.style.overflow='visible'
-               }
-          
-     });
-    // открытие и закрыьте фильтра
-    const filterNode = document.querySelector('.filter')
-    const filterPopupNode = document.querySelector('.filter-popup')
-     const opacityContent = document.querySelector('.popup-filter')
-     window.addEventListener('click', (e) => {
-      if (filterNode.contains(e.target)) {
-   filterPopupNode.classList.toggle('filter-popup--js-toggle')
-        opacityContent.classList.toggle('popup-filter--js-active')
-      }else {
-         filterPopupNode.classList.remove('filter-popup--js-toggle')
-         opacityContent.classList.remove('popup-filter--js-active')
-      }
+window.addEventListener('click', function (e) {
+   if (basketNode.contains(e.target) || (basketBodyNode.contains(e.target))) {
+      basketPopupNode.classList.add('basket-popup--js-open')
+      document.body.style.overflow = 'hidden'
+   } else {
+      basketPopupNode.classList.remove('basket-popup--js-open')
+      document.body.style.overflow = 'visible'
+   }
+
+});
+
+//увеличение и уменьшение товара в корзине
+window.addEventListener('click', (event) => {
+   let amout;
+   if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+      const wrapperCounter = event.target.closest('.basket-popup__product-amout-w')
+      amout = wrapperCounter.querySelector('.basket-popup__product-amout')
+   }
+   if (event.target.dataset.action === 'plus') {
+      amout.innerText = ++amout.innerText
+   }
+   if (event.target.dataset.action === 'minus') {
+      if (parseInt(amout.innerText) > 1)
+         amout.innerText = --amout.innerText
+   }
+
+   if (event.target.hasAttribute('data-action') && event.target.closest('.basket-popup__products')) {
+      calc()
+   }
+})
+
+
+//   подсчет товара в корзине
+function calc() {
+   const productItems = document.querySelectorAll('.basket-popup__product')
+   let totalPrice = 0;
+   const totalPriceEl = document.querySelector('.basket-popup__price')
+
+   productItems.forEach(function (elem) {
+      const amoutEl = elem.querySelector('[data-amout]')
+      const priceEl = elem.querySelector('.basket-popup__product-price')
+      const currentPrice = parseInt(amoutEl.innerHTML) * parseInt(priceEl.innerHTML);
+      totalPrice += currentPrice
+
+
+   })
+   totalPriceEl.innerText = totalPrice + '₽'
+
+}
+
+// количество товара в корзине
+const productsNode = document.querySelector('.basket-popup__products')
+const productBasketStatus = () => {
+
+   const countProductsNode = document.querySelector('.header__basket-count')
+   const countBasketProductsNode = document.querySelector('.basket-popup__amount-products')
+   countProductsNode.innerText = productsNode.children.length
+   countBasketProductsNode.innerText = (productsNode.children.length + ' товара')
+   console.log(productsNode.children.length)
+}
+// удаление товара из корзины
+window.addEventListener('click', (event) => {
+   if (event.target.dataset.close === 'del') {
+      event.target.closest('.basket-popup__product').remove();
+      productBasketStatus()
+      calc()
+   }
+})
+
+// удаление всех товаров из корзины
+const clearProductsBsket = () => {
+   const clearProduct = document.querySelector('.basket-popup__clear-products')
+   clearProduct.addEventListener('click', () => {
+      const activeProduct = document.querySelectorAll('.basket-popup__product')
+      activeProduct.forEach(function (elem) {
+         elem.remove()
       })
-     
-     
-      const filterItemLink = document.querySelectorAll('.filter-popup__nav-item-link')
-        const filterValue = document.querySelector('.filter__name')  
-      filterItemLink.forEach(function(e) {
-             e.addEventListener('click', function() {
-               filterValue.innerText = this.innerText
-               filterItemLink.forEach(function(e){
-               e.classList.remove('filter-popup__nav-item-link--active')
-               })
-               e.classList.add('filter-popup__nav-item-link--active')
-             })
-         
-            })
+      const countProductsNode = document.querySelector('.header__basket-count')
+      const countBasketProductsNode = document.querySelector('.basket-popup__amount-products')
+      countProductsNode.innerText = productsNode.children.length
+      countBasketProductsNode.innerText = (productsNode.children.length + ' товара')
+      calc()
+   })
+}
+clearProductsBsket()
+
+
+
+// открытие и закрытие фильтра
+const filterNode = document.querySelector('.filter')
+const filterPopupNode = document.querySelector('.filter-popup')
+const opacityContent = document.querySelector('.popup-filter')
+window.addEventListener('click', (e) => {
+   if (filterNode.contains(e.target)) {
+      filterPopupNode.classList.toggle('filter-popup--js-toggle')
+      opacityContent.classList.toggle('popup-filter--js-active')
+   } else {
+      filterPopupNode.classList.remove('filter-popup--js-toggle')
+      opacityContent.classList.remove('popup-filter--js-active')
+   }
+})
+
+
+const filterItemLink = document.querySelectorAll('.filter-popup__nav-item-link')
+const filterValue = document.querySelector('.filter__name')
+filterItemLink.forEach(function (e) {
+   e.addEventListener('click', function () {
+      filterValue.innerText = this.innerText
+      filterItemLink.forEach(function (e) {
+         e.classList.remove('filter-popup__nav-item-link--active')
+      })
+      e.classList.add('filter-popup__nav-item-link--active')
+   })
+
+})
